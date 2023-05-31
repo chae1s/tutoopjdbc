45b3bd1a7235ee73971ddd9f68c0435b95a6193e
chae1s <chae1zd.b@gmail.com>
2023-05-31 11:38:07 +0900

    fix : sqlite - jdbc 연결

--- src/DBMain.java
+++ src/DBMain.java
@@ -2,15 +2,16 @@ import java.sql.*;
 
 public class DBMain {
     public static void main(String[] args) {
-        String sql = "select 1 + 1 as result";
+        String sql = "select code, name from country order by code, name";
         Connection conn = null;
         try {
-            conn = DriverManager.getConnection("jdbc:sqlite:sample.db");
+            conn = DriverManager.getConnection("jdbc:sqlite:world.db");
             PreparedStatement psmt = conn.prepareStatement(sql);
             ResultSet rs = psmt.executeQuery();
 
             while (rs.next()) {
-                System.out.println(rs.getString("result"));
+                System.out.print(rs.getString("code") + " ");
+                System.out.println(rs.getString("name"));
             }
 
         } catch (SQLException e) {
