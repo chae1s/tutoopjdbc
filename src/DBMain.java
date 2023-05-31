5e262cdda734983c453ea68d0ce10d9441416cdd
chae1s <chae1zd.b@gmail.com>
2023-05-31 13:28:11 +0900

    feat : 반복되는 부분 상속으로 구현

--- src/DBMain.java
+++ src/DBMain.java
@@ -1,29 +1,54 @@
+import model.BaseDAO;
+
 import java.sql.*;
 
-public class DBMain {
+public class DBMain extends BaseDAO {
     public static void main(String[] args) {
-        String sql = "select code, name from country order by code, name";
-        Connection conn = null;
+        DBMain dbMain = new DBMain();
+        dbMain.getCodeName();
+    }
+
+    private void initPerson() {
         try {
-            conn = DriverManager.getConnection("jdbc:sqlite:world.db");
-            PreparedStatement psmt = conn.prepareStatement(sql);
-            ResultSet rs = psmt.executeQuery();
+            getConn();
+            smt = conn.createStatement();
+            smt.setQueryTimeout(30);
+
+            smt.executeUpdate("drop table if exists person");
+            smt.executeUpdate("create table person (id integer, name string)");
+            smt.executeUpdate("insert into person values(1, 'leo')");
+            smt.executeUpdate("insert into person values(2, 'yui')");
+            rs = smt.executeQuery("select * from person");
 
             while (rs.next()) {
-                System.out.print(rs.getString("code") + " ");
+                System.out.println("name = " + rs.getString("name"));
+                System.out.println("id = " + rs.getInt("id"));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        finally {
+            close();
+        }
+    }
+
+    private void getCodeName() {
+        String sql = "select code, name from country order by code, name";
+        try {
+            getConn();
+            psmt = conn.prepareStatement(sql);
+            rs = psmt.executeQuery();
+            while(rs.next()){
+                System.out.print(rs.getString("code") + "\t");
                 System.out.println(rs.getString("name"));
             }
 
         } catch (SQLException e) {
-            throw new RuntimeException(e);
-        } finally {
-            if (conn != null) {
-                try {
-                    conn.close();
-                } catch (SQLException e) {
-                    throw new RuntimeException(e);
-                }
-            }
+            e.printStackTrace();
         }
+        finally {
+            close();
+        }
+
     }
 }
