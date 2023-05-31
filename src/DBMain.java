2e370f8ee189da12f1da6a2ead2f80fa4a9209de
chae1s <chae1zd.b@gmail.com>
2023-05-31 13:48:35 +0900

    feat : table id 자동 증가, table 생성, 추가 메소드 분리

--- src/DBMain.java
+++ src/DBMain.java
@@ -5,31 +5,50 @@ import java.sql.*;
 public class DBMain extends BaseDAO {
     public static void main(String[] args) {
         DBMain dbMain = new DBMain();
-        dbMain.getCodeName();
+        dbMain.initPerson();
+        dbMain.insertPerson("nana");
+        dbMain.insertPerson("choonsik");
     }
 
     private void initPerson() {
+        String sql = """
+                create table person (
+                id integer primary key autoincrement, 
+                name String
+                )
+                """;
         try {
             getConn();
             smt = conn.createStatement();
-            smt.setQueryTimeout(30);
 
             smt.executeUpdate("drop table if exists person");
-            smt.executeUpdate("create table person (id integer, name string)");
-            smt.executeUpdate("insert into person values(1, 'leo')");
-            smt.executeUpdate("insert into person values(2, 'yui')");
-            rs = smt.executeQuery("select * from person");
-
-            while (rs.next()) {
-                System.out.println("name = " + rs.getString("name"));
-                System.out.println("id = " + rs.getInt("id"));
-            }
+            smt.executeUpdate(sql);
         } catch (Exception e) {
             e.printStackTrace();
         }
         finally {
             close();
         }
+    }
+
+    private int insertPerson(String name) {
+        int count = 0;
+        String sql = "insert into person(name) values(?)";
+        try {
+            getConn();
+            psmt = conn.prepareStatement(sql);
+            psmt.setString(1, name);
+            count = psmt.executeUpdate();
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        finally {
+            close();
+        }
+
+        return count;
     }
 
     private void getCodeName() {
